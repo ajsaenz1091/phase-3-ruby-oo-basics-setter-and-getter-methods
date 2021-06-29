8de026b0161c67bb817b5e9bb3be186125ee2597
Alberto <ajsaenz1091@gmail.com>
2021-06-29 13:43:53 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # Setter method for dog's name
+    def name=(name)
+        @name = name
+    end
 
+    #Getter method for dog's name
+    def name
+        @name
+    end
+
+     # Setter method for dog's breed
+     def breed=(breed)
+        @breed = breed
+    end
+
+    #Getter method for dog's breed
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
-class Person
+class Person 
+    # Setter method for person's name
+    def name=(name)
+        @name = name
+    end
 
+    #Getter method for person's name
+    def name
+        @name
+    end
+
+    # Setter method for person's job
+    def job=(job)
+        @job = job
+    end
+
+    #Getter method for person's job
+    def job
+        @job
+    end
 end
